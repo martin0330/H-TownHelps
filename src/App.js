70dac43379c5d0dc1c6b5218d79ac64bec519d73
Yash Patel <1pebble@Yashs-MacBook-Air.local>
2024-09-19 18:28:13 -0500

    Added the notification page and the volunteer history page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import VolunteerMatchingForm from "./pages/volunteer-match";
+import React from 'react';
+import NotificationPage from "./pages/NotificationPage"; // Adjust the path based on your folder structure
+import VolunteerMatchingForm from "./pages/volunteer-match"; // Adjust the path based on your folder structure
+import VolunteerHistory from "./pages/volunteer-hist"; // Adjust the path based on your folder structure
 
-
-
-function App() {
+const App = () => {
   return (
-    <div className = "App">
+    <div className="container mx-auto p-8">
+      {/* Display Volunteer Matching Form */}
       <VolunteerMatchingForm />
+
+      {/* Display Volunteer History */}
+      <VolunteerHistory />
+
+      {/* Display Notification Page */}
+      <NotificationPage />
     </div>
   );
-}
+};
 
 export default App;
+
